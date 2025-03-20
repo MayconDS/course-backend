76b8d5f2d748782697a0000203f5551cc6df776d
Maycon <crossfireelite601@gmail.com>
2025-03-19 23:26:20 -0300

    first commit2

--- src/environments/environment.prod.js
+++ src/environments/environment.prod.js
@@ -1,5 +1,9 @@
 // const ProdEnvironment = {
-//   db_uri: "mongodb://localhost:27017/course-backend",
+
+// };
+// module.exports = { ProdEnvironment };
+
+// db_uri: "mongodb://localhost:27017/course-backend",
 //   sendgrid: {
 //     api_key:
 //       "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",
@@ -8,5 +12,3 @@
 //   jwt_secret_key: "secretkeydev",
 //   KEYMP:
 //     "APP_USR-7811419267009284-101914-49614075aa04e9fcb67dd7200735d2c0-1429874750",
-// };
-// module.exports = { ProdEnvironment };
